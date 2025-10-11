accf0733c8c6443dc847c2ee536f371ee990876d
martin reda <martinreda999@outlook.com>
2025-10-12 02:58:06 +0300

    02 Entities relation Configurations

---
+++ GymManagementDAL/Data/Configurations/GymUserConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using GymManagementDAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Data.Configurations
+{
+    internal class GymUserConfiguration<T> : IEntityTypeConfiguration<T> where T : GymUser
+    {
+        public void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.Property(X => X.Name)
+                .HasColumnType("varchar")
+                .HasMaxLength(50);
+
+            builder.Property(X => X.Email)
+              .HasColumnType("varchar")
+              .HasMaxLength(100);
+            builder.Property(X => X.Email)
+             .HasColumnType("varchar")
+             .HasMaxLength(11);
+
+
+            builder.ToTable(Tb =>
+            {
+                Tb.HasCheckConstraint("GymValidEmailCheck", "Email Like '_%@_%._%'");
+                Tb.HasCheckConstraint("GymValidPhoneCheck", "Phone Like '01%' and Phone not Like '%[^0-9]%'");
+            });
+            builder.HasIndex(X => X.Email).IsUnique();
+            builder.HasIndex(X => X.Phone).IsUnique();
+
+            builder.OwnsOne(X => X.Address, AddressBuilder =>
+            {
+                AddressBuilder.Property(X => X.Street)
+                .HasColumnType("varchar")
+                .HasColumnName("Street")
+                .HasMaxLength(30);
+
+                AddressBuilder.Property(X => X.City)
+             .HasColumnType("varchar")
+             .HasColumnName("City")
+             .HasMaxLength(30);
+
+                AddressBuilder.Property(X => X.BulidingNumber)
+             .HasColumnName("BulidingNumber");
+             
+            });
+        }
+    }
+}
