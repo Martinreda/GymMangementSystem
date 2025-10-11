39f1eef6d8932f9576d728e8fd9502081fbd35f1
martin reda <martinreda999@outlook.com>
2025-10-12 02:04:38 +0300

    EntityCore

--- GymManagementDAL/Entities/Session.cs
+++ GymManagementDAL/Entities/Session.cs
@@ -12,5 +12,6 @@ namespace GymManagementDAL.Entities
         public int Capacity { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+
     }
 }
