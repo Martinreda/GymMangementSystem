b72e13107915a941b0e28084d95acb0256998e3b
martin reda <martinreda999@outlook.com>
2025-10-15 16:05:35 +0300

    Task Add Entities  Repositories

---
+++ GymManagementDAL/Repositiories/Interfaces/IHealthRecordRepository.cs
@@ -0,0 +1,19 @@
+﻿using GymManagementDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositiories.Interfaces
+{
+    internal interface IHealthRecordRepository
+    {
+        IEnumerable<HealthRecord> GetALl();
+        HealthRecord? GetById(int Id);
+
+        int Add(HealthRecord healthRecord);
+        int Update(HealthRecord healthRecord);
+        int Delete (HealthRecord healthRecord);
+    }
+}
---
+++ GymManagementDAL/Repositiories/Interfaces/IMemberSessionRepository.cs
@@ -0,0 +1,19 @@
+﻿using GymManagementDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositiories.Interfaces
+{
+    public interface IMemberSessionRepository
+    {
+        IEnumerable<MemberSession> GetALl();
+        MemberSession? GetById(int Id);
+
+        int Add(MemberSession memberSession);
+        int Update(MemberSession memberSession);
+        int Delete(MemberSession memberSession);
+    }
+}
