41434c9f93fac65a80f0ced3cd73f989938cf091
martin reda <martinreda999@outlook.com>
2025-10-17 03:52:48 +0300

    03 Generic Repository

---
+++ GymManagementDAL/Repositiories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using GymManagementDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositiories.Interfaces
+{
+    internal interface IGenericRepository<TEntity> where TEntity : BaseEntity , new()
+    {
+        TEntity? GetById(int Id);
+        IEnumerable<TEntity> GetALl();
+        int Add(TEntity entity);
+        int Update(TEntity entity);
+        int Delete(TEntity entity);
+    }
+}
---
+++ GymManagementDAL/Repositiories/Interfaces/IPlanRepository.cs
@@ -0,0 +1,16 @@
+﻿using GymManagementDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositiories.Interfaces
+{
+    internal interface IPlanRepository
+    {
+        Plan? GetById(int Id);
+        IEnumerable<Plan> GetAll();
+        int Update(Plan plan);
+    }
+}
