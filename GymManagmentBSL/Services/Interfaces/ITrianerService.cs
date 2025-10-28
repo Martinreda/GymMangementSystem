257e7f52601c5540484956938feb7cb145f63cfd
martin reda <martinreda999@outlook.com>
2025-10-28 16:11:19 +0300

    13 Task Add Trainer Services

---
+++ GymManagmentBSL/Services/Interfaces/ITrianerService.cs
@@ -0,0 +1,20 @@
+﻿using GymManagmentBSL.ViewModels.TrianerViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GymManagmentBSL.Services.Interfaces
+{
+    internal interface ITrianerService
+    {
+        IEnumerable<TrainerViewModel> GetAllTrainers();
+        bool CreateTrainer(CreateTrainerViewModel createdTrainer);
+        TrainerViewModel? GetTrainerDetails(int trainerId);
+        TrainerToUpdateViewModel? GetTrainerToUpdate(int trainerId);
+        bool UpdateTrainerDetails(TrainerToUpdateViewModel updatedTrainer, int trainerId);
+        bool RemoveTrainer(int trainerId);
+    }
+}
