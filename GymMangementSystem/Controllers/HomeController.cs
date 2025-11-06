9ec6352644777279452ba202461121a3979b0f35
martin reda <martinreda999@outlook.com>
2025-11-06 04:37:38 +0200

    Action Return Type

--- GymMangementSystem/Controllers/HomeController.cs
+++ GymMangementSystem/Controllers/HomeController.cs
@@ -1,32 +1,44 @@
-using System.Diagnostics;
-using GymMangementSystem.Models;
+﻿using GymManagementDAL.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymMangementSystem.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public JsonResult Trainers()
         {
-            return View();
+            var Trainers = new List<Trainer>()
+        {
+            new Trainer() { Name = "Mohamed", Phone = "12345678" },
+            new Trainer() { Name = "Amr", Phone = "812355" }
+        };
+
+            return Json(data: Trainers);
+        }
+        public RedirectResult Redirect()
+        {
+            return Redirect("https://github.com/Martinreda/GymMangementSystem/tree/Demo03");
+        }
+        public ContentResult Content()
+        {
+            return Content("Hello rom GMS");
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public FileResult DownloadFile()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var FilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "css", "site.css");
+            var FileBytes = System.IO.File.ReadAllBytes(path: FilePath);
+
+            return File(fileContents: FileBytes, contentType: "text/css", fileDownloadName: "DownloadableSite.css");
+        }
+
+        public EmptyResult EmptyResult()
+        {
+            return new EmptyResult();
         }
     }
 }
